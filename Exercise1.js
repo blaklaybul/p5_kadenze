faf33e2191e3fbb8ae635209a31ab9d8e3303c70
Michael Hirsch <michael.hirsch@rga.com>
2015-11-09 10:28:47 -0500

    no submission

--- Exercise1.js
+++ Exercise1.js
@@ -1,9 +1,15 @@
-var width = 1024;
-var height = 768;
-createCanvas(width, height);
-background(0);
+function setup() {
+  // uncomment this line to make the canvas the full size of the window
+  var width = 1024;
+  var height = 768;
+  createCanvas(width, height);
+  background(0);
+}
 
-fill(133,49,129);
-stroke(19,29,212);
-strokeWeight(10);
-ellipse(width/2, height/2, 60, 60 );
+function draw() {
+  // draw stuff here
+  fill(133,49,129);
+  stroke(19,29,212);
+  strokeWeight(10)
+  ellipse(width/2, height/2, 60, 60 );
+}
